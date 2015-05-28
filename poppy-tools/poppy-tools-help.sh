9f8150941f21b756d049e584184bcc15caf647bb
Nicolas <nicolas.rabault@inria.fr>
2015-05-28 09:36:29 +0200

    poppy-tools helper

---
+++ poppy-tools/poppy-tools-help.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo -e "${bold}NOM${normal}"
+echo -e "\tpoppy-tools"
+
+echo -e "${bold}SYNOPSYS${normal}"
+echo -e "\tpoppy-tools [option] [args]"
+
+echo -e "${bold}DESCRIPTION${normal}"
+echo -e "\tpoppy-tools provide command line access to your robot tools."
+
+echo -e "${bold}OPTIONS${normal}"
+echo -e "\t${bold}install${normal}\tDefine your architecture (linux board, poppy creature) and update your poppy tools"
+echo -e "\t${bold}update ${normal}\tUpdate your poppy tools"
+echo -e "\t${bold}help   ${normal}\tPrint this manual"
