c89daed2daa04ecc98e4ce10a4efb8ae0f188313
Nicolas <nicolas.rabault@inria.fr>
2015-05-28 11:55:33 +0200

    Manage config file and launch script

---
+++ poppy-tools/poppy-tools-update.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+wget -P /tmp/ https://raw.githubusercontent.com/poppy-project/poppy-installer/master/conf/$POPPY_BOARD/$POPPY_CREATURE/install.conf
+
+while IFS=: read name file_link
+do
+   echo -e "\e[33m$name instalation: \e[0m"
+   curl -L $file_link | sudo bash
+done < /tmp/install.conf
