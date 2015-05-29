ad4f61da4a74645079c1219f5d43fd478cb1c072
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-05-29 18:06:22 +0200

    Make sure a command is provided to poppy-tools.

--- poppy-tools.sh
+++ poppy-tools.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
 command_name=$1
+
+if [ -z "$command_name" ];
+ then
+  echo "please try 'poppy-tools help' command";
+  exit
+fi
+
 shift
 bash /home/poppy/dev/poppy-tools/poppy-tools-$command_name.sh $@ || echo "please try 'poppy-tools help' command"
