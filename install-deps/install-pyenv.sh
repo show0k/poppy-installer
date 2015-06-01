1e2a3fe4c92f9da024c39eef3c9be85f4386963a
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-01 16:11:48 +0200

    Add a first dependency to the rpi/ergo-jr/install con to check if everything works well.

---
+++ install-deps/install-pyenv.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+sudo apt-get install -y curl git
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm
+sudo apt-get install -y libfreetype6-dev libpng++-dev
+
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+echo "
+export PATH=\"\$HOME/.pyenv/bin:\$PATH\"
+eval \"\$(pyenv init -)\"
+eval \"\$(pyenv virtualenv-init -)\"" >> $HOME/.bashrc
