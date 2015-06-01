2cd4bd396ae20de6f638d9dff8fa5e738c5c6a88
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-01 16:24:39 +0200

    Change the separator used in the install.conf from : to " "

--- poppy-tools/poppy-tools-update.sh
+++ poppy-tools/poppy-tools-update.sh
@@ -2,7 +2,7 @@
 
 wget -P /tmp/ https://raw.githubusercontent.com/poppy-project/poppy-installer/master/conf/$POPPY_BOARD/$POPPY_CREATURE/install.conf
 
-while IFS=: read name file_link
+while IFS=" " read name file_link
 do
    echo -e "\e[33m$name instalation: \e[0m"
    curl -L $file_link | sudo bash
