274368c92bada680f6f4e555a60188d8d10d99fc
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-02 12:58:10 +0200

    just trying...

--- install-deps/install-python-std-deps.sh
+++ install-deps/install-python-std-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y liblapack-dev libblas-dev gfortran
+# sudo apt-get install -y liblapack-dev libblas-dev gfortran
 
 pip install pip -U
 pip install numpy -U
