0cfaa95b15572cb0f8c3ddec651bc2fda1101794
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-02 11:35:27 +0200

    Run the install script as poppy.

--- poppy-tools/poppy-tools-update.sh
+++ poppy-tools/poppy-tools-update.sh
@@ -5,5 +5,5 @@ wget https://raw.githubusercontent.com/poppy-project/poppy-installer/master/conf
 while IFS=" " read name file_link
 do
    echo -e "\e[33m$name instalation: \e[0m"
-   curl -L $file_link | sudo bash
+   curl -L $file_link | bash
 done < /tmp/install-poppy.conf
