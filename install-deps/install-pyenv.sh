10b3036fcbaa545d9b302e9355dd2621ff847520
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-02 11:36:40 +0200

    Update the install pyenv script so it can be re-run without problem

--- install-deps/install-pyenv.sh
+++ install-deps/install-pyenv.sh
@@ -1,16 +1,18 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-sudo apt-get install -y curl git
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm
-sudo apt-get install -y libfreetype6-dev libpng++-dev
+if [ ! -d "$HOME/.pyenv" ]; then
+  sudo apt-get install -y curl git
+  sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm
+  sudo apt-get install -y libfreetype6-dev libpng++-dev
 
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+  curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+  export PATH="$HOME/.pyenv/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
 
-echo "
-export PATH=\"\$HOME/.pyenv/bin:\$PATH\"
-eval \"\$(pyenv init -)\"
-eval \"\$(pyenv virtualenv-init -)\"" >> $HOME/.bashrc
+  echo "
+  export PATH=\"\$HOME/.pyenv/bin:\$PATH\"
+  eval \"\$(pyenv init -)\"
+  eval \"\$(pyenv virtualenv-init -)\"" >> $HOME/.bashrc
+fi
