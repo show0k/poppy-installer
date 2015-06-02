9c2acd190fe6895f7091bdd94f28cbb4cd0ae854
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-02 12:39:20 +0200

    We should now use bash_profile to declare variables used by other dependencies.

--- install-deps/install-python2.7.10.sh
+++ install-deps/install-python2.7.10.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sed -i /PYTHON_VERSION/d $HOME/.bashrc
-echo "export PYTHON_VERSION=2.7.10" >> $HOME/.bashrc
+echo "export PYTHON_VERSION=2.7.10" >> $HOME/.bash_profile
 export PYTHON_VERSION=2.7.10
 
 if [ ! -d "$HOME/.pyenv/versions/$PYTHON_VERSION" ]; then
