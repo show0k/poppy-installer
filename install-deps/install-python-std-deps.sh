46c63e2ab19cdf02040a1398542a9e87c1f9658d
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-02 11:34:51 +0200

    Add pip and scipy as default python-deps.

--- install-deps/install-python-std-deps.sh
+++ install-deps/install-python-std-deps.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y liblapack-dev libblas-dev gfortran
+
+pip install pip -U
 pip install numpy -U
 pip install matplotlib -U
 pip install ipython[all] -U
+pip install scipy -U
