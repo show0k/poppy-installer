81798b3669cf12df881baba4411833006ad9c0d1
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-07-02 12:10:54 +0200

    Update install-python-std-deps.sh

--- install-deps/install-python-std-deps.sh
+++ install-deps/install-python-std-deps.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y python-numpy python-scipy python-matplotlib
 
-pip install pip -U
-pip install numpy -U
-pip install matplotlib -U
-pip install ipython[all] -U
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+rm get-pip.py
 
-sudo apt-get install -y liblapack-dev libblas-dev gfortran
-pip install scipy -U
+sudo apt-get install libzmq-dev
+
+if hash pip 2>/dev/null; then
+  sudo pip install pip -U
+else
+  wget https://bootstrap.pypa.io/get-pip.py
+  sudo python get-pip.py
+  rm get-pip.py
+fi
