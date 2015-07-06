10ea466d8ff290a557e26dda5fca8e0a84f9fa93
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-07-06 17:47:02 +0200

    Update poppy-configure.sh

--- poppy-configure.sh
+++ poppy-configure.sh
@@ -57,9 +57,9 @@ chmod -w /etc/sudoers
 usermod --pass='*' root # don't need root password anymore
 
 # Download and run poppy-tools install
-su - poppy -c "wget -P /home/poppy/ https://raw.githubusercontent.com/poppy-project/poppy-installer/master/poppy-tools.sh"
+su - pi -c "wget -P /home/poppy/ https://raw.githubusercontent.com/poppy-project/poppy-installer/master/poppy-tools.sh"
 mv /home/poppy/poppy-tools.sh /usr/local/bin/poppy-tools
 chmod +x /usr/local/bin/poppy-tools
-su - poppy -c "mkdir /home/poppy/dev /home/poppy/dev/poppy-tools"
+su - pi -c "mkdir /home/poppy/dev /home/poppy/dev/poppy-tools"
 
-su - poppy -c "poppy-tools install $*"
+su - pi -c "poppy-tools install $*"
